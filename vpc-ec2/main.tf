7ee7cab928b677f489d6539dbcec8a3e904582fd
chucks <iamdanielchukwuma@gmail.com>
2025-05-30 02:42:28 +0100

    Create variable

--- vpc-ec2/main.tf
+++ vpc-ec2/main.tf
@@ -1,19 +1,6 @@
 provider "aws" {
   region = "eu-central-1"
 }
-
-variable vpc_cidr_block {}
-variable subnet_cidr_block {}
-variable avail_zone {}
-variable avail_zone-2 {}
-variable env_prefix {}
-variable ingress_ip {}
-variable my_ip {}
-variable instance_type {}
-variable public_key_location {}
-variable private_key_location {}
-variable subnet_2_cidr_block {}
-
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
   tags = {
---
+++ vpc-ec2/variables.tf
@@ -0,0 +1,13 @@
+/*Clearing up terraform workspace*/
+
+variable vpc_cidr_block {}
+variable subnet_cidr_block {}
+variable avail_zone {}
+variable avail_zone-2 {}
+variable env_prefix {}
+variable ingress_ip {}
+variable my_ip {}
+variable instance_type {}
+variable public_key_location {}
+variable private_key_location {}
+variable subnet_2_cidr_block {}
